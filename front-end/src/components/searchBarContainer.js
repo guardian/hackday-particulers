004c7b9cbc87650b8377ad3260cac19327b1af22
Raphael Kabo <raphaelkabo@hey.com>
2022-05-11 15:51:59 +0100

    Added subtitle

--- front-end/src/components/searchBarContainer.js
+++ front-end/src/components/searchBarContainer.js
@@ -3,13 +3,18 @@ import Container from "react-bootstrap/Container";
 import SearchBar from "./searchBar";
 import logo from "../logo.png";
 
-const SearchBarContainer = ({fetch}) => {
+const SearchBarContainer = ({ fetch }) => {
   return (
     <div className="container">
       <div className="p-5 bg-light rounded-3 d-flex align-items-center justify-content-between">
-      <img src={logo} alt="Logo" className="logo" />
+        <div>
+          <img src={logo} alt="Logo" className="logo" />
+          <p className="lead fs-4">
+            <em>Whose article is it anyway?</em>
+          </p>
+        </div>
         <div className="search-bar-container">
-          <SearchBar fetch={fetch}/>
+          <SearchBar fetch={fetch} />
         </div>
       </div>
     </div>
